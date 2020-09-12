9b1b5e79602acf31d8ee703d291e463cd43fa168
wang_glb <819278056_glb@qq.com>
2020-09-12 19:39:16 +0800

    更新1

--- HelloGit/src/main/java/com/atguifu/git/HelloGit.java
+++ HelloGit/src/main/java/com/atguifu/git/HelloGit.java
@@ -5,6 +5,7 @@ import javax.xml.bind.SchemaOutputResolver;
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("Hello Git");
+        System.out.println("更新1");
     }
 
 }
